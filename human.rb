a735b110cb21b72effe1d75940af847fef63e848
cokeman <cokeredder@gmail.com>
2021-05-24 10:39:09 +0000

    kadai-ruby-oop-3

---
+++ human.rb
@@ -0,0 +1,14 @@
+require './animal.rb'
+require './thinkable.rb'
+
+class Human < Animal
+    
+    attr_accessor :hobby
+    
+    def initialize(name,age,hobby)
+        super(name,age)
+        self.hobby = hobby
+    end
+    
+    include Thinkable
+end
